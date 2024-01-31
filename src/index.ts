cbfe2b8f6d9b01fc2ae12f82c6c4f14e1c39b3c7
Austen Stone <austenstone@github.com>
2024-01-31 12:40:35 -0500

    Add support for JSON artifact conversion to CSV

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
+import { DefaultArtifactClient } from "@actions/artifact";
 import { setOutput, getInput } from "@actions/core";
+import { readFileSync } from "fs";
 import { json2csv } from "json-2-csv";
 
 interface Input {
   token: string;
   json: object[];
+  jsonArtifactName: string;
   options: object;
 }
 
-export function getInputs(): Input {
+const getInputs = async (): Promise<Input> => {
   const result = {} as Input;
   result.token = getInput("token");
-  result.json = JSON.parse(getInput("json"));
+  result.jsonArtifactName = getInput("json-artifact-name");
+  if (result.jsonArtifactName) {
+    const artifact = new DefaultArtifactClient()
+    const artifactResponse = await artifact.getArtifact(result.jsonArtifactName);
+    if (!artifactResponse) throw new Error(`Artifact ${result.jsonArtifactName} not found`);
+    const { downloadPath } = await artifact.downloadArtifact(artifactResponse.artifact.id)
+    if (!downloadPath) throw new Error(`Artifact ${result.jsonArtifactName} failed to download`);
+    result.json = JSON.parse(readFileSync(downloadPath, "utf8"));
+  } else {
+    result.json = JSON.parse(getInput("json"));
+  }
   const options = getInput("options");
   result.options = options ? JSON.parse(getInput("options")) : undefined;
-  return result;
+  return new Promise((resolve) => resolve(result));
 }
 
-const inputs = getInputs();
+const inputs = await getInputs();
 const csv = await json2csv(inputs.json, inputs.options);
 setOutput("csv", csv.replace(/'/g, "\\'"));
