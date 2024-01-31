e04002f7a19351c2474263afc7b3d6bc5824b110
Austen Stone <austenstone@github.com>
2024-01-31 11:22:28 -0500

    Remove unused code and artifacts creation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-import { setOutput, getInput, getBooleanInput } from "@actions/core";
+import { setOutput, getInput } from "@actions/core";
 import { json2csv } from 'json-2-csv';
-import { DefaultArtifactClient } from '@actions/artifact';
-import { writeFileSync } from "fs";
 
 interface Input {
   token: string;
   json: any;
   options: any;
-  createArtifact: boolean;
-  artifactName: string;
 }
 
 export function getInputs(): Input {
@@ -17,17 +13,9 @@ export function getInputs(): Input {
   result.json = JSON.parse(getInput("json"));
   const options = getInput("options");
   result.options = options ? JSON.parse(getInput("options")) : undefined;
-  result.createArtifact = getBooleanInput("create-artifact");
-  result.artifactName = getInput("artifact-name");
   return result;
 }
 
 const inputs = getInputs();
 const csv = await json2csv(inputs.json, inputs.options);
 setOutput("csv", csv);
-if (inputs.createArtifact) {
-  const fileName = 'output.csv';
-  writeFileSync(fileName, csv);
-  const artifact = new DefaultArtifactClient();
-  await artifact.uploadArtifact(inputs.artifactName, [fileName], '.');
-}
