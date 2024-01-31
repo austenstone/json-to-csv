30c511ec60ba24b128495da087fdee298564c0db
Austen Stone <austenstone@github.com>
2024-01-31 13:26:44 -0500

    Add support for specifying the path to the JSON artifact

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { DefaultArtifactClient } from "@actions/artifact";
 import { setOutput, getInput } from "@actions/core";
-import { readdirSync } from "fs";
+import { mkdirSync, readFileSync, readdirSync } from "fs";
 import { json2csv } from "json-2-csv";
 
 interface Input {
@@ -15,19 +15,22 @@ const getInputs = async (): Promise<Input> => {
   result.token = getInput("token");
   result.jsonArtifactName = getInput("json-artifact-name");
   if (result.jsonArtifactName) {
+    const tmpPath = '.json-to-csv';
     const artifact = new DefaultArtifactClient()
     const artifactResponse = await artifact.getArtifact(result.jsonArtifactName);
-    if (!artifactResponse) throw new Error(`Artifact ${result.jsonArtifactName} not found`);
-    const { downloadPath } = await artifact.downloadArtifact(artifactResponse.artifact.id)
-    if (!downloadPath) throw new Error(`Artifact ${result.jsonArtifactName} failed to download`);
-    console.log(`Artifact ${result.jsonArtifactName} downloaded to ${downloadPath}`);
-    // show files at downloadPath reading dir
-
-    console.log(`Reading ${downloadPath}`);
-    readdirSync(downloadPath).forEach(file => {
-      console.log(file);
+    mkdirSync(tmpPath, { recursive: true });
+    const { downloadPath } = await artifact.downloadArtifact(artifactResponse.artifact.id, {
+      path: tmpPath
     });
-
+    if (!downloadPath) throw new Error(`Artifact ${result.jsonArtifactName} not found`);
+    console.log(`Artifact ${result.jsonArtifactName} downloaded to ${downloadPath}`);
+    const jsonFiles = readdirSync(downloadPath).find((file) => file.endsWith('.json'));
+    if (!jsonFiles) {
+      throw new Error(`No JSON files found in artifact ${result.jsonArtifactName}`);
+    } else if (jsonFiles?.length > 1) {
+      console.warn(`Found ${jsonFiles.length} JSON files, using the first one`);
+    }
+    result.json = JSON.parse(readFileSync(jsonFiles[0], 'utf-8'));
   } else {
     result.json = JSON.parse(getInput("json"));
   }
